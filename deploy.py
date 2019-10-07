fb501b38f2c2696cb13072f141cf49d1ba3ea564
Jamie <lydiaxi.wow@gmail.com>
2019-10-07 17:31:14 +0800

    implant get

--- deploy.py
+++ deploy.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_restplus import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
+
+@api.route('/language')
+class Language(Resource):
+    def get(self):
+        return {'hey' : 'there'}
+
+
+@app.route('/home')
 def index():
-    return '<h1>Deployed to Heroku!!!</h1>'
-
+    return "done!"
 
 if __name__ == "__main__":
     app.run()
